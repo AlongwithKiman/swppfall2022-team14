3114215a5a2601bafde5a374b1106d35422523fd
AlongwithKiman <expfunsce@snu.ac.kr>
2023-11-03 15:12:46 +0900

    fix:ec2 endpoint

--- backend/qualla/qualla/settings/production.py
+++ backend/qualla/qualla/settings/production.py
@@ -3,7 +3,8 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['tipsybackend.shop','qualla.kro.kr',\
-                 'ec2-15-164-129-199.ap-northeast-2.compute.amazonaws.com', '13.125.144.254','ec2-15-164-95-176.ap-northeast-2.compute.amazonaws.com', 'ec2-13-125-227-250.ap-northeast-2.compute.amazonaws.com','15.164.224.81','ec2-15-164-224-81.ap-northeast-2.compute.amazonaws.com']
+                 '13.209.77.169','ec2-13-209-77-169.ap-northeast-2.compute.amazonaws.com',\
+                 '13.125.144.254','ec2-15-164-95-176.ap-northeast-2.compute.amazonaws.com', 'ec2-13-125-227-250.ap-northeast-2.compute.amazonaws.com','15.164.224.81','ec2-15-164-224-81.ap-northeast-2.compute.amazonaws.com']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent

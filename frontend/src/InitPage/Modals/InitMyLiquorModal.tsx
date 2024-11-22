0dfbd64b65ed2a7ccbae7b7812a89501dd059a54
AlongwithKiman <expfunsce@snu.ac.kr>
2024-11-22 21:39:11 +0900

    fix: 나의 술장 팝업 문구 변경

--- frontend/src/InitPage/Modals/InitMyLiquorModal.tsx
+++ frontend/src/InitPage/Modals/InitMyLiquorModal.tsx
@@ -4,7 +4,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import {fetchMyIngredientList, selectIngredient} from '../../store/slices/ingredient/ingredient';
 import IngredientItem from '../../common/Components/IngredientItem';
 import Modal from '@mui/material/Modal';
-import { Grid, Stack } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import {selectUser} from "../../store/slices/user/user";
 import {AppDispatch} from "../../store";
 
@@ -46,6 +46,27 @@ const InitMyLiqourModal = (props: prop) => {
             onClose={() => setIsOpen(false)}
         >
             <Stack sx={style}>
+            <Typography
+                variant="h3"
+                sx={(theme) => ({
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    mt: 1, // 위쪽 여백
+                    mb: 2, // 아래쪽 여백 (필요에 따라 조정)
+                    [theme.breakpoints.down("md")]: {
+                    fontSize: 30,
+                    },
+                    [theme.breakpoints.down("sm")]: {
+                    fontSize: 20,
+                    },
+                })}
+                >
+                내 재료 목록
+            </Typography>
+
+
                 <Grid container columns={4} spacing={3}>
                     {ingredientState.myIngredientList.map(ingredient => (
                         <Grid key={ingredient.id} item md={1} sm={2} xs={4}>

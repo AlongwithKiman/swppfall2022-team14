9b30bbcb62d9a6a72c227016a9a1cee9b5d4b2f6
AlongwithKiman <expfunsce@snu.ac.kr>
2024-11-22 21:58:44 +0900

    fix: 칵테일 삭제 오류 대응을 위해  parent_comment 삭제 시 규칙 protected--> set_null로 변경

--- backend/qualla/comment/models.py
+++ backend/qualla/comment/models.py
@@ -11,10 +11,10 @@ class Comment(models.Model):
     author_id = models.IntegerField(
         null=True, default=None)  # Null when deleted
 
-    # Do not delete when parent deleted
-    # Actually does not happen
+    # 대댓글 기능 구현을 위해, comment의 delete is_deleted flag를 0으로 set하게 구현되어 있다.
+    # 따라서 일반적인 댓글 삭제 상황시 대댓글은 유지되나, Cocktail 객체가 삭제될 시에는 하위 댓글이 모두 삭제되므로 그 아래 대댓글은 SET_NULL로 처리하면 충분하다.
     parent_comment = models.ForeignKey(
-        'self', on_delete=models.PROTECT, related_name='replies', null=True)
+        'self', on_delete=models.SET_NULL, related_name='replies', null=True)
 
     content = models.CharField(max_length=500, null=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False)
